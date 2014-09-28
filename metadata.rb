5ceff35bd69056b68da4428d045f7b086dfed762
Kenton Newby <kentonnewby@gmail.com>
2014-09-27 22:02:04 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "nginx-chef"
+maintainer        "Mix Digital Media, LLC"
+maintainer_email  "info@mixdigitalmedia.com"
+description       "Installs the nginx web server from the nginx ppa"
+version           "0.0.1"
+
+recipe "nginx-chef", "nginx server"
+
+supports "ubuntu"
